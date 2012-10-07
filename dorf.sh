2d5bdd48e6736be32d3569a735da48cf99f7eaba
user <user@hyperion.delta>
2012-10-07 19:09:41 +0200

    initial commit

---
+++ dorf.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+xset s off
+xset -dpms
+
+(
+while true; do
+wkhtmltopdf --page-size A3 --margin-top 0 --minimum-font-size 20 \
+ 'https://wiki.chaosdorf.de/Infodisplay' /tmp/info.pdf
+pdftoppm -png -y 140 -H 1000 -r 109 /tmp/info.pdf > /tmp/termine.png
+wkhtmltopdf --page-size A3 --margin-top 0 --minimum-font-size 20 \
+ 'http://chaosdorf.de/' /tmp/news.pdf
+pdftoppm -png -y 260 -H 1000 -r 109 /tmp/news.pdf > /tmp/news.png
+sleep 1h
+done
+) &
+
+(
+while true; do
+feh --full-screen --slideshow-delay 30 --hide-pointer --cycle-once \
+'http://vrrf.finalrewind.org/Duesseldorf/Luisenstr.png?no_lines=10' \
+/tmp/termine.png \
+/tmp/news.png
+done
+) &
