a10f0eac4916c30c66d2ac864591dc684979c8f7
user <user@hyperion.delta>
2012-10-08 14:30:23 +0200

    add CSS override for wiki footer

--- dorf.sh
+++ dorf.sh
@@ -6,9 +6,11 @@ xset -dpms
 (
 while true; do
 wkhtmltopdf --page-size A3 --margin-top 0 --minimum-font-size 20 \
+ --user-style-sheet wiki-override.css \
  'https://wiki.chaosdorf.de/Infodisplay' /tmp/info.pdf
 pdftoppm -png -y 140 -H 1000 -r 109 /tmp/info.pdf > /tmp/termine.png
 wkhtmltopdf --page-size A3 --margin-top 0 --minimum-font-size 20 \
+ --user-style-sheet blog-override.css \
  'http://chaosdorf.de/' /tmp/news.pdf
 pdftoppm -png -y 260 -H 1000 -r 109 /tmp/news.pdf > /tmp/news.png
 sleep 1h
